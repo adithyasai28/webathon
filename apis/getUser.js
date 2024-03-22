f48ea49d99b4ca517875633f90e69b0623cbb239
Adithya <adithyasairam19@gmail.com>
2024-03-22 18:48:15 +0530

    updated getUser.js

--- apis/getUser.js
+++ apis/getUser.js
@@ -1,23 +1,26 @@
 const express = require("express")
 const userapi = express.Router()
+
 userapi.use(express.json())
+
 const verifyToken = require('../middlewares/verifyToken')
 const bcryptjs = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const expressAsyncHandler = require("express-async-handler")
 
 
-let userCollection, allVehecleCollection, allStarHotelCollection, allPlaceCollection, allTransportCollection;
-userapi.use((req, res) => {
-    userCollection = req.app.get("userCollection")
-    allVehecleCollection = req.app.get("allVehecleCollection")
-    allStarHotelCollection = req.app.get("allStarHotelCollection")
-    allPlaceCollection = req.app.get("allPlaceCollection")
-    allTransportCollection = req.app.get("allTransportCollection")
-    next()
-})
+// let userCollection, allVehecleCollection, allStarHotelCollection, allPlaceCollection, allTransportCollection;
+// userapi.use((req, res) => {
+//     userCollection = req.app.get("userCollection")
+//     allVehecleCollection = req.app.get("allVehecleCollection")
+//     allStarHotelCollection = req.app.get("allStarHotelCollection")
+//     allPlaceCollection = req.app.get("allPlaceCollection")
+//     allTransportCollection = req.app.get("allTransportCollection")
+//     next()
+// })
 userapi.post("/signup", expressAsyncHandler(async (req, res) => {
 
+    let userCollection = req.app.get("userCollection")
     let newuser = req.body
     let user = await userCollection.findOne({ username: newuser.username })
     if (!user) {
@@ -33,6 +36,7 @@ userapi.post("/signup", expressAsyncHandler(async (req, res) => {
 
 userapi.post("/signin", expressAsyncHandler(async (req, res) => {
 
+    let userCollection = req.app.get("userCollection")
     let loginuser = req.body
     let userDB = await userCollection.findOne({ username: loginuser.username })
     if (userDB) {
@@ -50,7 +54,7 @@ userapi.post("/signin", expressAsyncHandler(async (req, res) => {
 }));
 
 userapi.get("/getTransport", expressAsyncHandler(async (req, res) => {
-
+    let allTransportCollection = req.app.get("allTransportCollectio")
     let transportUser = req.body
     let transportDB = await allTransportCollection.find({ destination: transportUser.destination, 
                                                           typeOfTransport: transportUser.typeOfTransport, 
@@ -59,15 +63,27 @@ userapi.get("/getTransport", expressAsyncHandler(async (req, res) => {
 }))
 
 userapi.get("/getVehicles", expressAsyncHandler(async (req, res) => {
-
+    let allVehecleCollection = req.app.get("allVehecleCollection")
+    let vehecleUser = req.body
+    let vehecleDB = await allVehecleCollection.find({ destination : vehecleDB.destination,
+                                                      vehecleType : vehecleDB.vehecleType }).toArray();
+    res.status(201).send({data : vehecleDB})                                                 
 }))
 
 userapi.get("/getHotels", expressAsyncHandler(async (req, res) => {
+    let allHotelCollection = req.app.get("allHotelCollection")
+    let hotelUser = req.body
+    let hotelDB = await allHotelCollection.find({ destination : hotelDB.destination,
+                                                  starRating : hotelDB.starRating}).toArray();
+    res.status(201).send({data : hotelDB})                                                 
 
 }))
 
 userapi.get("/getPlaces", expressAsyncHandler(async (req, res) => {
-
+    let allPlaceCollection = req.app.get("allPlaceCollection")
+    let placeUser = req.body
+    let placeDB = await allPlaceCollection.find({ destination : placeDB.destination}).toArray();
+    res.status(201).send({data : placeDB}) 
 }))
 
 
